2c7185bd232d41414b5327fdfb81cbfea0e5412f
RyleighSharpley-Dev <140826736+RyleighSharpley-Dev@users.noreply.github.com>
2024-05-06 12:48:31 +0200

    Adde migration of course and quiz relationship successfully

---
+++ LearniVerseNew/Migrations/202405061047346_Added Relationships between course and quiz.cs
@@ -0,0 +1,24 @@
+﻿namespace LearniVerseNew.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedRelationshipsbetweencourseandquiz : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Quizs", "CourseID", c => c.Guid(nullable: false));
+            AddColumn("dbo.Quizs", "Course_CourseID", c => c.String(maxLength: 128));
+            CreateIndex("dbo.Quizs", "Course_CourseID");
+            AddForeignKey("dbo.Quizs", "Course_CourseID", "dbo.Courses", "CourseID");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Quizs", "Course_CourseID", "dbo.Courses");
+            DropIndex("dbo.Quizs", new[] { "Course_CourseID" });
+            DropColumn("dbo.Quizs", "Course_CourseID");
+            DropColumn("dbo.Quizs", "CourseID");
+        }
+    }
+}
