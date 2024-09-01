d0e1e1665cf48ca2cf99c19af9778139c72c8bc7
RyleighSharpley-Dev <140826736+RyleighSharpley-Dev@users.noreply.github.com>
2024-09-01 17:39:09 +0200

    Added the Helper class to search for exercises

---
+++ LearniVerseNew/Models/ApplicationModels/Regimen Models/ExerciseResponse.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearniVerseNew.Models.ApplicationModels.Regimen_Models
+{
+    public class ExerciseResponse
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("muscle")]
+        public string Muscle { get; set; }
+
+        [JsonProperty("equipment")]
+        public string Equipment { get; set; }
+
+        [JsonProperty("instructions")]
+        public string Instructions { get; set; }
+    }
+}
