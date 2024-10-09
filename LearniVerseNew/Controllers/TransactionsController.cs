765ebaa8a334ff10cba8a953724c0f194164f9aa
RyleighSharpley-Dev <140826736+RyleighSharpley-Dev@users.noreply.github.com>
2024-10-09 20:13:27 +0200

    Scaffolded basic views to manage transactions

---
+++ LearniVerseNew/Controllers/TransactionsController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LearniVerseNew.Models;
+using LearniVerseNew.Models.ApplicationModels.Store_Models;
+
+namespace LearniVerseNew.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Transactions
+        public async Task<ActionResult> Index()
+        {
+            var transactions = db.Transactions.Include(t => t.Order);
+            return View(await transactions.ToListAsync());
+        }
+
+        // GET: Transactions/Details/5
+        public async Task<ActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transaction transaction = await db.Transactions.FindAsync(id);
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaction);
+        }
+
+        // GET: Transactions/Create
+        public ActionResult Create()
+        {
+            ViewBag.OrderID = new SelectList(db.Orders, "OrderID", "StudentID");
+            return View();
+        }
+
+        // POST: Transactions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "TransactionID,OrderID,Amount,Status,TransactionDate,PaystackReference")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                transaction.TransactionID = Guid.NewGuid();
+                db.Transactions.Add(transaction);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.OrderID = new SelectList(db.Orders, "OrderID", "StudentID", transaction.OrderID);
+            return View(transaction);
+        }
+
+        // GET: Transactions/Edit/5
+        public async Task<ActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transaction transaction = await db.Transactions.FindAsync(id);
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.OrderID = new SelectList(db.Orders, "OrderID", "StudentID", transaction.OrderID);
+            return View(transaction);
+        }
+
+        // POST: Transactions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "TransactionID,OrderID,Amount,Status,TransactionDate,PaystackReference")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(transaction).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.OrderID = new SelectList(db.Orders, "OrderID", "StudentID", transaction.OrderID);
+            return View(transaction);
+        }
+
+        // GET: Transactions/Delete/5
+        public async Task<ActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transaction transaction = await db.Transactions.FindAsync(id);
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaction);
+        }
+
+        // POST: Transactions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(Guid id)
+        {
+            Transaction transaction = await db.Transactions.FindAsync(id);
+            db.Transactions.Remove(transaction);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
